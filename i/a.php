d63059b3082c47db893d8d0532a3ec92ef48788f
witthawat-26 <66010914037@msu.ac.th>
2026-02-17 18:40:00 +0700

    ฟ

--- i/a.php
+++ i/a.php
@@ -7,5 +7,30 @@
 </head>
 <body>
     <h1>วิทวัส วงศ์ภาคำ(เกม)</h1>
+
+
+<table border="1">
+    <tr>
+        <th>รหัสภาค</th>
+        <th>ชื่อภาค</th>
+        <th>ลบ</th>
+
+    </tr>
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM 'regions'";
+$rs = mysqli_qery($conn,$sql);
+
+while($data = mysqli_fetch_array($rs)){
+?>
+    <tr>
+        <td><?php echo $data['r_id'];?></td>
+        <td><?php echo $data['r_name'];?></td>
+        <td width="80" align="ceter"> <img src="images/delete.jpg" width="20"></td>
+
+    </tr>
+<?php} ?>
+</table> 
+
 </body>
 </html>
