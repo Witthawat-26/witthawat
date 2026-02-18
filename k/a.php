247f7a958d60ee8d309b2ae5bdbc08010b3a8f94
Witthawat-26 <66010914037@msu.ac.th>
2026-02-18 17:15:30 +0700

    ฟ

---
+++ k/a.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>งาน k 66010914037 วิทวัส วงศ์ภาคำ</title>
+</head>
+<body>
+    <h1>งาน k 66010914037 วิทวัส วงศ์ภาคำ</h1>
+<button onclick="showImage('1.jpg', this)" style="background-color:green; color:white; padding:10px; border:none;">
+    อยากโดน
+</button>
+<button onclick="showImage('2.jpg', this)" style="background-color:orange; color:white; padding:10px; border:none;">
+    อยากโดนเปิด
+</button>
+<script>
+
+function showImage(imgSrc, btn){
+    btn.innerHTML = "<img src='" + imgSrc + "' width='150'>";
+}
+</script>
+
+    
+</body>
+</html>
