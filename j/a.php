71131d06843d2956105a00a6c9c1bb61f371565a
Witthawat-26 <66010914037@msu.ac.th>
2026-02-18 14:00:37 +0700

    a

---
+++ j/a.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>วิทวัส วงศ์ภาคำ (เกม)</title>
+</head>
+<body>
+    <h1>วิทวัส วงศ์ภาคำ (เกม)</h1>
+    img src=""
+    <td><img src="images/1.jpg" width="20"></td>
+    
+</body>
+</html>
